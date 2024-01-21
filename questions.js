322f0edc899d96c15d64312b9f35ba2e9d3c6c63
Sakshi Jaiswal <144778485+thesakshijaiswal@users.noreply.github.com>
2024-01-21 09:03:25 +0530

    Added questions to be solved in the quiz app

---
+++ questions.js
@@ -0,0 +1,113 @@
+let questions = [
+  {
+    num: 1,
+    question: "What does HTML stands for?",
+    answer: "C.Hyper Text Markup Language",
+    option: [
+      "A.Hyper Type Multi Language",
+      "B.Hyper Text Multiple Language",
+      "C.Hyper Text Markup Language",
+      "D. Home Text Multi Language"
+    ]
+  },
+  {
+    num: 2,
+    question: "What does CSS stands for?",
+    answer: "A.Cascading Style Sheet",
+    option: [
+      "A.Cascading Style Sheet",
+      "B.Cute Style Sheet ",
+      "C.Computer Style Sheet",
+      "D.Compressed Style Sheet"
+    ]
+  },
+  {
+    num: 3,
+    question: "What does PHP stands for?",
+    answer: "B.Hypertext PreProcessor",
+    option: [
+      "A.Home text preprocessor",
+      "B.Hypertext PreProcessor",
+      "C.Hypertext PreProgramming",
+      "D.Pre Hypertext Processor"
+    ]
+  },
+  {
+    num: 4,
+    question: "What does SQL stands for?",
+    answer: "D.Structural Query Language",
+    option: [
+      "A.Stylesheet Query Language",
+      "B.Strength Query Language",
+      "C.Simple Query Language",
+      "D.Structural Query Language"
+    ]
+  },
+  {
+    num: 5,
+    question: "What does XML stands for?",
+    answer: "A.Extensible Markup Language",
+    option: [
+      "A.Extensible Markup Language",
+      "B.External Markup Language",
+      "C.Extented Multi Language",
+      "D.Extra Markup Language"
+    ]
+  },
+  {
+    num: 6,
+    question: "What Express js is?",
+    answer: "C.A npm module / library",
+    option: [
+      "A.A framework",
+      "B.Extended javascript",
+      "C.A npm module / library",
+      "D.A query language"
+    ]
+  },
+  {
+    num: 7,
+    question: "What does JsON stands for?",
+    answer: "A.Javascript Object Notation",
+    option: [
+      "A.Javascript Object Notation",
+      "B.Javascript and Node",
+      "C.Java object Notation",
+      "D.Java and node"
+    ]
+  },
+  {
+    num: 8,
+    question: "What does SDLC stands for?",
+    answer: "B.Software Development Life Cycle",
+    option: [
+      "A.Software Development Licence",
+      "B.Software Development Life Cycle",
+      "C.Soft Document Licence",
+      "D.Software Decoding Life cycle"
+    ]
+  },
+  {
+    num: 9,
+    question: "What does .jsx in react stands for?",
+    answer: "D.Javascript and XML",
+    option: [
+      "A.Javascript and ML",
+      "B.Extended Javascript",
+      "C.Javascript extension",
+      "D.Javascript and XML"
+    ]
+  },
+  {
+    num: 10,
+    question: "What is Object?",
+    answer: "B.Instance of Class",
+    option: [
+      "A.Collection of items",
+      "B.Instance of Class",
+      "C.Instance of Method",
+      "D.Instance of Array"
+    ]
+  }
+
+];
